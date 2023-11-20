c8fbc08630c4f9864c916d2135130c588f014b32
starspacezhou <128214282+starspacezhou@users.noreply.github.com>
2023-11-20 12:52:55 +0800

    参考

---
+++ 锋迷商城-全套笔记代码/锋迷商城-前后端分离-笔记代码/1-前后端分离/相关技术点代码/前端技术点源码/vue/vue/js/music.js
@@ -0,0 +1,878 @@
+var data = {
+	"result": {
+		"songs": [
+			{
+				"id": 25640392,
+				"name": "淋雨一直走",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 2262033,
+					"name": "有形的翅膀",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1349971200007,
+					"size": "11",
+					"copyrightId": "684010",
+					"status": "3",
+					"picId": 559651418577639,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 204906,
+				"copyrightId": 684010,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 108,
+				"fee": 8,
+				"mark": 512,
+				"rurl": null
+			}, {
+				"id": 139808,
+				"name": "快乐崇拜",
+				"artists": [
+					{
+						"id": 4723,
+						"name": "潘玮柏",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}, {
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 13935,
+					"name": "WU HA",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1094169600000,
+					"size": "10",
+					"copyrightId": "7003",
+					"status": "3",
+					"picId": 109951165549352126,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 205306,
+				"copyrightId": 7003,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 8192,
+				"rurl": null
+			}, {
+				"id": 1494462204,
+				"name": "快乐崇拜",
+				"artists": [
+					{
+						"id": 4723,
+						"name": "潘玮柏",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}, {
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 98156671,
+					"name": "Your Uncle's Favourite KTV Songs That You Secretly Like",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1605024000000,
+					"size": "28",
+					"copyrightId": "7003",
+					"status": "3",
+					"picId": 109951165460311280,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 205348,
+				"copyrightId": 7003,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 1408983926,
+				"name": "作势装腔",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 84037191,
+					"name": "？",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1576598400000,
+					"size": "10",
+					"copyrightId": "1416428",
+					"status": "0",
+					"picId": 109951164553007725,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 228592,
+				"copyrightId": 1416428,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 8192,
+				"rurl": null
+			}, {
+				"id": 421160957,
+				"name": "不害怕",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 34751905,
+					"name": "全面沦陷",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1469030400000,
+					"size": "11",
+					"copyrightId": "698010",
+					"status": "3",
+					"picId": 109951163052842348,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 231115,
+				"copyrightId": 698010,
+				"status": 0,
+				"alias": [
+					"原曲：Never Give Up"
+				],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 5341349,
+				"fee": 1,
+				"mark": 8192,
+				"rurl": null
+			}, {
+				"id": 25640409,
+				"name": "有形的翅膀",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 2262033,
+					"name": "有形的翅膀",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1349971200007,
+					"size": "11",
+					"copyrightId": "684010",
+					"status": "3",
+					"picId": 559651418577639,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 216106,
+				"copyrightId": 684010,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 28185,
+				"fee": 8,
+				"mark": 512,
+				"rurl": null
+			}, {
+				"id": 36871866,
+				"name": "隐形的翅膀 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 3414029,
+					"name": "隐藏的歌手 第六期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1448121600007,
+					"size": "4",
+					"copyrightId": "0",
+					"status": "0",
+					"picId": 3405187513733087,
+					"alia": [
+						"现场音频实录"
+					],
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 220641,
+				"copyrightId": 0,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 0,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 36841953,
+				"name": "亲爱的那不是爱情 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 3414029,
+					"name": "隐藏的歌手 第六期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1448121600007,
+					"size": "4",
+					"copyrightId": "0",
+					"status": "0",
+					"picId": 3405187513733087,
+					"alia": [
+						"现场音频实录"
+					],
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 277394,
+				"copyrightId": 0,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 0,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 36841951,
+				"name": "欧若拉 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 3414029,
+					"name": "隐藏的歌手 第六期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1448121600007,
+					"size": "4",
+					"copyrightId": "0",
+					"status": "0",
+					"picId": 3405187513733087,
+					"alia": [
+						"现场音频实录"
+					],
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 117605,
+				"copyrightId": 0,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 0,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 422132010,
+				"name": "把你信仰",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 34751905,
+					"name": "全面沦陷",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p1.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1469030400000,
+					"size": "11",
+					"copyrightId": "698010",
+					"status": "3",
+					"picId": 109951163052842348,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 304076,
+				"copyrightId": 698010,
+				"status": 0,
+				"alias": [
+					"电视剧《寻找爱的冒险》片尾曲"
+				],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 5341351,
+				"fee": 1,
+				"mark": 8192,
+				"rurl": null
+			}, {
+				"id": 36841952,
+				"name": "遗失的美好 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 3414029,
+					"name": "隐藏的歌手 第六期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1448121600007,
+					"size": "4",
+					"copyrightId": "0",
+					"status": "0",
+					"picId": 3405187513733087,
+					"alia": [
+						"现场音频实录"
+					],
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 276026,
+				"copyrightId": 0,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 0,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 1830688947,
+				"name": "念念 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 124759371,
+					"name": "为歌而赞 第2期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1616169600000,
+					"size": "6",
+					"copyrightId": "2707435",
+					"status": "0",
+					"picId": 109951165816590704,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 249193,
+				"copyrightId": 2707435,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 262144,
+				"rurl": null
+			}, {
+				"id": 1826422220,
+				"name": "心恋 (Live)",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 124189379,
+					"name": "为歌而赞 第1期",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1615564800000,
+					"size": "5",
+					"copyrightId": "2707435",
+					"status": "0",
+					"picId": 109951165794012198,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 200698,
+				"copyrightId": 2707435,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 0,
+				"rurl": null
+			}, {
+				"id": 1342798235,
+				"name": "天分",
+				"artists": [
+					{
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 75535120,
+					"name": "我的奇妙男友2之恋恋不忘 音乐原声带",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1550160000000,
+					"size": "5",
+					"copyrightId": "1412830",
+					"status": "0",
+					"picId": 109951163858972977,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 263396,
+				"copyrightId": 1412830,
+				"status": 0,
+				"alias": [
+					"电视剧《我的奇妙男友2》主题曲"
+				],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 8,
+				"mark": 8192,
+				"rurl": null
+			}, {
+				"id": 108257,
+				"name": "保护色",
+				"artists": [
+					{
+						"id": 3684,
+						"name": "林俊杰",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}, {
+						"id": 10562,
+						"name": "张韶涵",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					}
+				],
+				"album": {
+					"id": 10755,
+					"name": "她说 概念自选辑",
+					"artist": {
+						"id": 0,
+						"name": "",
+						"picUrl": null,
+						"alias": [],
+						"albumSize": 0,
+						"picId": 0,
+						"img1v1Url": "http://p2.music.126.net/6y-UleORITEDbvrOLV0Q8A==/5639395138885805.jpg",
+						"img1v1": 0,
+						"trans": null
+					},
+					"publishTime": 1291737600000,
+					"size": "14",
+					"copyrightId": "22036",
+					"status": "1",
+					"picId": 109951163071284933,
+					"alia": null,
+					"mark": "0",
+					"type": null,
+					"blurPicUrl": null,
+					"pic": 0,
+					"picUrl": null,
+					"description": null,
+					"tags": null,
+					"company": null
+				},
+				"duration": 199000,
+				"copyrightId": 22036,
+				"status": 0,
+				"alias": [],
+				"rtype": 0,
+				"ftype": 0,
+				"mvid": 0,
+				"fee": 1,
+				"mark": 8192,
+				"rurl": null
+			}
+		],
+		"songCount": 600
+	},
+	"code": 200
+};
